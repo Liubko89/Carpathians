ff37d65b8676eb0a53d2bf0a2b375c09e87ca763
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2024-11-08 19:06:08 +0000

    finished reviews section

---
+++ src/components/ReviewTemplate/ReviewTemplate.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import css from "./ReviewTemplate.module.css";
+import { firstReview } from "../../helpers/reviews";
+import icons from "../../assets/icons.svg";
+
+const ReviewTemplate = ({ image, photo }) => {
+  const [overlay, setOverlay] = useState(false);
+  return (
+    <li className={css.listItem}>
+      <div className={css.wrapper}>
+        <div className={css.content}>
+          <img className={css.img} src={image} alt="reviewer" />
+          <h3 className={css.listTitle}>Maria</h3>
+          <p className={css.descr}>{firstReview}</p>
+        </div>
+        <button className={css.btn} onClick={() => setOverlay(!overlay)}>
+          <svg className={css.icon} width="26" height="16">
+            <use href={`${icons}#icon-round`}></use>
+          </svg>
+        </button>
+        <div
+          className={
+            overlay ? `${css.turnArround} ${css.overlay}` : css.overlay
+          }
+        >
+          <img className={css.overlayImg} src={photo} alt="photo" />
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default ReviewTemplate;
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,42 +1,17 @@
 import css from "./Reviews.module.css";
 import imgMaria from "../../assets/img/mob1x/Ellipse30.png";
-import imgOstap from "../../assets/img/mob1x/Ellipse31.jpg";
+import imgOstap from "../../assets/img/mob1x/Ellipse31.png";
 import photoMaria from "../../assets/img/mob1x/Rectangle94.jpg";
 import photoOstap from "../../assets/img/mob1x/Rectangle93.jpg";
-import { firstReview } from "../../helpers/reviews";
-import { useState } from "react";
-import icons from "../../assets/icons.svg";
+import ReviewTemplate from "../ReviewTemplate/ReviewTemplate";
 
 const Reviews = () => {
-  const [overlay, setOverlay] = useState(false);
-
   return (
     <section className={css.section}>
       <h2 className={css.title}>Reviews</h2>
       <ul className={css.list}>
-        <li className={css.listItem}>
-          <div className={css.wrapper}>
-            <div className={css.content}>
-              <img className={css.img} src={imgMaria} alt="reviewer" />
-              <h3 className={css.listTitle}>Maria</h3>
-              <p className={css.descr}>{firstReview}</p>
-            </div>
-            <button className={css.btn} onClick={() => setOverlay(!overlay)}>
-              <svg className={css.icon} width="26" height="16">
-                <use href={`${icons}#icon-round`}></use>
-              </svg>
-            </button>
-            <div
-              className={
-                overlay ? `${css.turnArround} ${css.overlay}` : css.overlay
-              }
-            >
-              <img className={css.overlayImg} src={photoMaria} alt="photo" />
-            </div>
-          </div>
-        </li>
-        <li></li>
-        <li className={css.lastReview}>last review</li>
+        <ReviewTemplate image={imgMaria} photo={photoMaria} />
+        <ReviewTemplate image={imgOstap} photo={photoOstap} />
       </ul>
     </section>
   );
