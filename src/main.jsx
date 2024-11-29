82a257d6efde320d30dfd586d6c032ab6b2ed1a2
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2024-11-29 21:53:37 +0000

    some small changes in css

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import "modern-normalize";
 import App from "./App.jsx";
 import "./css/reset.css";
 import "./css/fonts.css";
-import "./css/index.css";
 import "./css/variables.css";
+import "./css/index.css";
 import "./css/additional.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
