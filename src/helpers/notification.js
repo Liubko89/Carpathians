c6299d86f983159447c048f5fb2df9adb85649e0
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2025-01-21 13:32:30 +0000

    some corrections

--- src/helpers/notification.js
+++ src/helpers/notification.js
@@ -1,7 +1,9 @@
 import toast from "react-hot-toast";
+import i18n from "../utils/i18n";
+const { t } = i18n;
 
 export const notify = (values) => {
-  toast(`Thank you ${values.name}! We will call you soon`, {
+  toast(t("notify", { username: values.name }), {
     style: {
       borderRadius: "10px",
       background: "var(--modal-bg-c)",
